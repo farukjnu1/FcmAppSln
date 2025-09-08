b6d9958c34003b1348683c67a1483072afbe558c
farukjnu1@gmail.com <farukjnu1@gmail.com>
2025-09-08 14:42:38 +0800

    Add project files.

---
+++ FcmApp/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace FcmApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtDevice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtShortDesc = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIncidentNo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(259, 280);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(83, 33);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send to FCM";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtDevice
+            // 
+            this.txtDevice.Location = new System.Drawing.Point(12, 24);
+            this.txtDevice.Multiline = true;
+            this.txtDevice.Name = "txtDevice";
+            this.txtDevice.Size = new System.Drawing.Size(348, 109);
+            this.txtDevice.TabIndex = 1;
+            this.txtDevice.Text = "cowntgz1BPo:APA91bHQsYVpwRW-lsAg9kvsa7C9f9hcBCCQv7XKcTD4h50wWVp8MaoQyIt5NRyde3SGf" +
+    "1k9N0OsaBSkpcTcQOyqu98KLRO0xnSSmMkYIbN7zZkc4J67EWHad6hMdYnHEyMKlXOeEUo4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Device ID:";
+            // 
+            // txtShortDesc
+            // 
+            this.txtShortDesc.Location = new System.Drawing.Point(72, 143);
+            this.txtShortDesc.Name = "txtShortDesc";
+            this.txtShortDesc.Size = new System.Drawing.Size(288, 20);
+            this.txtShortDesc.TabIndex = 3;
+            this.txtShortDesc.Text = "Some short desc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ShortDesc:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "IncidentNo:";
+            // 
+            // txtIncidentNo
+            // 
+            this.txtIncidentNo.Location = new System.Drawing.Point(72, 167);
+            this.txtIncidentNo.Name = "txtIncidentNo";
+            this.txtIncidentNo.Size = new System.Drawing.Size(288, 20);
+            this.txtIncidentNo.TabIndex = 5;
+            this.txtIncidentNo.Text = "any number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Description:";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(72, 191);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(288, 20);
+            this.txtDescription.TabIndex = 7;
+            this.txtDescription.Text = "detail desc";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Title:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(73, 216);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(288, 20);
+            this.txtTitle.TabIndex = 9;
+            this.txtTitle.Text = "ServiceNow: Incident No. number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 248);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Text:";
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(73, 245);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(288, 20);
+            this.txtText.TabIndex = 11;
+            this.txtText.Text = "This is Notification";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 325);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtIncidentNo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtShortDesc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDevice);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtDevice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtShortDesc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIncidentNo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtText;
+    }
+}
+
---
+++ FcmApp/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FcmApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string txtTo = txtDevice.Text.Trim();
+                string shortDesc = txtShortDesc.Text.Trim();
+                string incidentNo = txtIncidentNo.Text.Trim();
+                string description = txtDescription.Text.Trim();
+                string title = txtTitle.Text.Trim();
+                string text = txtText.Text.Trim();
+                string str = SendNotificationFromFirebaseCloud(txtTo
+                                                                , shortDesc
+                                                                , incidentNo
+                                                                , description
+                                                                , title
+                                                                , text);
+                MessageBox.Show("success: " + str);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        public static String SendNotificationFromFirebaseCloud(
+            string to
+            ,string ShortDesc
+            ,string IncidentNo
+            ,string Description
+            ,string title
+            ,string text)
+        {
+            var result = "-1";
+            var webAddr = "https://fcm.googleapis.com/fcm/send";
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(webAddr);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Headers.Add(HttpRequestHeader.Authorization, "key=TEST");
+            httpWebRequest.Method = "POST";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string strNJson = @"{
+                    ""to"": """+to+@""",
+                    ""data"": {
+                        ""ShortDesc"": """+ShortDesc+ @""",
+                        ""IncidentNo"": """ + IncidentNo + @""",
+                        ""Description"": """ + Description + @"""
+                          },
+                          ""notification"": {
+                                        ""title"": """ + title + @""",
+                            ""text"": """ + text + @""",
+                        ""sound"":""default""
+                          }
+                                }";
+                streamWriter.Write(strNJson);
+                streamWriter.Flush();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+            }
+            return result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
